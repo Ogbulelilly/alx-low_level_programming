a8e4977ec02fd9ecaa193b0e9a70b53dad30b90b
ogbulelilly <ogbulelilly@gmail.com>
2022-07-01 08:46:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+gcc -save-temps 4-puts.c -o 4-puts
+#include <studio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+puts ("\"programming is like building a multilingual puzzle");
+return (0);
+}
