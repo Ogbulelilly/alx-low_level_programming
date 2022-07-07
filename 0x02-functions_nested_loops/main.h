6f8fc60ab04785b67344f08338ab84b80d0d63a2
ogbulelilly <ogbulelilly@gmail.com>
2022-07-07 04:09:20 -0700

    task2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: main.h
- * Auth: Lilian
+ * Auth: Lilly
  * Desc: Header file containing declarations for all functions
  *       used in the 0x02-functions_nested_loops directory
  */
