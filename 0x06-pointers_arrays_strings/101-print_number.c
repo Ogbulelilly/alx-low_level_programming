afc780b49740a2da4513cacf300352d54cd44f5f
ogbulelilly <ogbulelilly@gmail.com>
2022-07-15 11:41:49 -0700

    task9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  print_number - prints an integer.
- *  @n: input integer.
- *  Return: no return.
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
 void print_number(int n)
 {
@@ -28,9 +28,9 @@ void print_number(int n)
 		count *= 10;
 	}
 
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+		for (; count >= 1; count /= 10)
+		{
+			_putchar(((m / count) % 10) + 48);
+		}
 }
-		
+
